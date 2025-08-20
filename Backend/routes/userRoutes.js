95de42eb78b3431bd0c00536b88bf5c8d7b90757
cindypatricia8 <138100735+cindypatricia8@users.noreply.github.com>
2025-08-20 22:14:53 +1000

    Test commit again

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// CRUD Routes
+// CRUD Routes for User
 router.get('/', userController.getAllUsers);
 
 router.get('/:id', userController.getUserById);
