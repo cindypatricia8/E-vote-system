0e2ff129364496c4d23e1b336c2f89c41a3199df
Celine <cgun0012@student.monash.edu>
2025-08-21 18:06:37 +1000

    Attempted to transfer html and css

---
+++ Frontend/src/pages/login.tsx
@@ -0,0 +1,19 @@
+import './login.css';
+
+export default function Login(){
+    return (
+        <div className="v1_2">
+        <div className="v6_25"></div><span className="v1_3">Sign In</span><span className="v5_12">Remember me</span>
+        <div className="v6_23"><span className="v5_7">Email</span><span className="v5_10">Password</span>
+            <div className="v5_8"></div>
+            <div className="v5_9"></div>
+        </div>
+        <div className="v6_15"></div><span className="v6_17">Forgot Password</span><span className="v6_20">Sign Up</span><span
+            className="v6_19">Don’t have an account? </span><span className="v6_24">Sign in with your Account to Access Monash
+            Portal Apps</span>
+        <div className="v6_37"></div><span className="v6_38">Sign In </span>
+        <div className="v14_11"></div>
+        <div className="v19_2"></div>
+    </div>
+    );
+}
