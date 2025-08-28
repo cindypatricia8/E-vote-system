1b25d241f10a893085dd3ac1b2765399e50556df
Celine <cgun0012@student.monash.edu>
2025-08-28 11:55:39 +1000

    Initial Login

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import Login from './pages/login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+
