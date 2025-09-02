e1199bd4a7e9a5e1e616e741d6cc2700880de494
cindypatricia8 <138100735+cindypatricia8@users.noreply.github.com>
2025-09-02 21:54:47 +1000

    Update electionRoutes

---
+++ Backend/routes/electionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const electionController = require('../controllers/electionController');
+const { protect } = require('../middleware/authMiddleware');
+
+// All election routes require a user to be logged in
+router.use(protect);
+
+router.post('/', electionController.createElection); // Permissions (clubAdmin) checked inside controller
+router.get('/active', electionController.getActiveElections);
+
+router.route('/:id')
+    .get(electionController.getElectionById)
+    .put(electionController.updateElection) // Permissions checked inside controller
+    .delete(electionController.deleteElection); // Permissions checked inside controller
+
+module.exports = router;
