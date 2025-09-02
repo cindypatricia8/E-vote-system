f98054bc6ddd8016410d48cdc6d45f7c3714fa28
cindypatricia8 <138100735+cindypatricia8@users.noreply.github.com>
2025-09-02 21:57:56 +1000

    Update index.js
    changed port number

--- Backend/index.js
+++ Backend/index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
