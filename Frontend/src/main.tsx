59e99111c43addbd4a6e9d1a514d83a8b51c5110
Celine <cgun0012@student.monash.edu>
2025-09-25 04:42:02 +1000

    testing implementation of carousel

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+// main.tsx or App.tsx
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
 //import App from './App.tsx'
 import Login from './pages/login.tsx'
+import Voting from './pages/voting.tsx'
+import SimpleSlider from './pages/test.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <SimpleSlider />
   </StrictMode>,
 )
 
